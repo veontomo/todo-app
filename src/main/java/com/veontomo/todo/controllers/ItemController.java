0a741d2f712f106171ff164ce00c460b88875c24
Andrew Shcherbakov <veontomo@gmail.com>
2019-04-13 18:53:14 +0200

    Show user info on the top

--- src/main/java/com/veontomo/todo/controllers/ItemController.java
+++ src/main/java/com/veontomo/todo/controllers/ItemController.java
@@ -1,7 +1,12 @@
 package com.veontomo.todo.controllers;
 
+import java.security.Principal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +14,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.veontomo.todo.model.Item;
 import com.veontomo.todo.persistence.ItemRepository;
+
 
 /**
  * CRUD for handling the todo items
@@ -20,6 +26,9 @@ import com.veontomo.todo.persistence.ItemRepository;
 public class ItemController {
 
     private final ItemRepository repo;
+    
+    private static Logger logger = LoggerFactory.getLogger(ItemController.class);
+
 
     @Autowired
     public ItemController(ItemRepository repo) {
@@ -27,9 +36,14 @@ public class ItemController {
     }
 
     @RequestMapping
-    public ModelAndView list() {
+    public ModelAndView list(Principal principal, ModelMap params) {
+        logger.info("Principal {}", principal.toString());
         final Iterable<Item> items = repo.getAll();
-        return new ModelAndView("items/list", "items", items);
+        params.addAttribute("items", items);
+        if (principal != null) {
+            params.addAttribute("userName", principal.getName());
+        }
+        return new ModelAndView("items/list");
     }
 
     @GetMapping("/new")
