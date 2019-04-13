e4320f62a17452754e0cf752635c5d260c2d1606
Andrew Shcherbakov <veontomo@gmail.com>
2019-04-13 20:26:11 +0200

    Add id to the ItemStatus

--- src/main/java/com/veontomo/todo/model/ItemStatus.java
+++ src/main/java/com/veontomo/todo/model/ItemStatus.java
@@ -6,13 +6,18 @@ package com.veontomo.todo.model;
  *
  */
 public enum ItemStatus {
-    TODO("to be done"),
-    INPROGRESS("in progress");
+    TODO(0, "to be done"),
+    INPROGRESS(1, "in progress"),
+    DONE(2, "done"),
+    REJECTED(3, "not worth doing");
+    
     
     public final String value;
+    public final int id;
     
-    ItemStatus(String value) {
+    ItemStatus(int id, String value) {
         this.value = value;
+        this.id = id;
     }
        
 }
