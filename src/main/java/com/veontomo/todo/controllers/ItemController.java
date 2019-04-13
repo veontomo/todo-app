e4d98837c50b08d2846805490dcb6cbbd152143f
Andrew Shcherbakov <veontomo@gmail.com>
2019-04-13 17:24:54 +0200

    Stub a method to create a new task

--- src/main/java/com/veontomo/todo/controllers/ItemController.java
+++ src/main/java/com/veontomo/todo/controllers/ItemController.java
@@ -2,11 +2,14 @@ package com.veontomo.todo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.veontomo.todo.model.Item;
 import com.veontomo.todo.persistence.ItemRepository;
+
 /**
  * CRUD for handling the todo items
  * @author Andrew
@@ -14,18 +17,24 @@ import com.veontomo.todo.persistence.ItemRepository;
  */
 @Controller
 @RequestMapping("/items")
-public class Items {
-    
+public class ItemController {
+
     private final ItemRepository repo;
-    
+
     @Autowired
-    public Items(ItemRepository repo) {
+    public ItemController(ItemRepository repo) {
         this.repo = repo;
     }
+
     @RequestMapping
     public ModelAndView list() {
         final Iterable<Item> items = repo.getAll();
         return new ModelAndView("items/list", "items", items);
+    }
+
+    @GetMapping("/new")
+    public ModelAndView create(@ModelAttribute Item item) {
+        return new ModelAndView("items/new");
     }
 
 }
