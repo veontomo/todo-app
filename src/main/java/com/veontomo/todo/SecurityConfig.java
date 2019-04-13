56ef6050638396873700e6e6b96cef265a96355c
Andrew Shcherbakov <veontomo@gmail.com>
2019-04-13 09:09:56 +0200

    Rename  Config into SecurityConfig

--- src/main/java/com/veontomo/todo/SecurityConfig.java
+++ src/main/java/com/veontomo/todo/SecurityConfig.java
@@ -2,17 +2,33 @@ package com.veontomo.todo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+/**
+ * Security configuration layer
+ * @author Andrew
+ *
+ */
 @EnableWebSecurity
-public class Config extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder authBuilder) throws Exception {
         authBuilder.inMemoryAuthentication()
             .withUser("user")
             .password("pass")
             .roles("USER");
+    }
+
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .formLogin()
+            .and()
+            .httpBasic();
     }
 
 }
--- src/main/java/com/veontomo/todo/TodoApplication.java
+++ src/main/java/com/veontomo/todo/TodoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TodoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(new Class[] {TodoApplication.class, Config.class}, args);
+        SpringApplication.run(new Class[] {TodoApplication.class, SecurityConfig.class}, args);
     }
  
 
