678fc0652d97973cb80346ccc6e543e739b777f2
Andrew Shcherbakov <veontomo@gmail.com>
2019-04-14 07:08:29 +0200

    Add API interface for managing tasks

---
+++ src/main/java/com/veontomo/todo/controllers/ITaskController.java
@@ -0,0 +1,78 @@
+package com.veontomo.todo.controllers;
+
+import java.security.Principal;
+import java.util.List;
+
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.veontomo.todo.model.Task;
+
+/**
+ * API for handling the tasks.
+ * @author Andrew
+ *
+ */
+public interface ITaskController {
+
+    /**
+     * Return a list of all tasks of all users.
+     * In case no tasks are present, an empty list should be returned.
+     * 
+     * Only ADMIN have an access to this endpoint. 
+     * @return
+     */
+    @GetMapping("/tasks")
+    @Secured("ADMIN")
+    public List<Task> getAll();
+
+    /**
+     * Get a list of all tasks of given user.
+     * In case non tasks are present, an empty list should be returned.
+     * 
+     * Only USER have an access to this endpoint.
+     * @param principal
+     * @return
+     */
+    @GetMapping("/tasks")
+    @Secured("USER")
+    public List<Task> getUsersTasks(Principal principal);
+
+    /**
+     * Save a task assigned to a given user.
+     * 
+     * The task to create and the user must have the same username. 
+     *  
+     * @param principal
+     * @param task
+     * @return a newly save task
+     */
+    @PostMapping("/tasks/save")
+    @Secured("USER")
+    public Task save(Principal principal, Task task);
+
+    /**
+     * Delete the task of the user. 
+     * 
+     * The task to delete and the user must have the same username. 
+     * @param principal
+     * @param task
+     * @return the deleted task or null if no task was found
+     */
+    @DeleteMapping("/tasks/delete")
+    @Secured("USER")
+    public Task deleteUserTask(Principal principal, Task task);
+
+    /**
+     * Delete the task.
+     * 
+     * Only ADMIN have an access to this endpoint. 
+     * @param task
+     * @return the deleted task or null if no task was found
+     */
+    @DeleteMapping("/tasks/delete")
+    @Secured("ADMIN")
+    public Task deleteTask(Task task);
+}
